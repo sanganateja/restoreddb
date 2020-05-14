dead9a39050396d52e2fd80b6005218ec02566bf
Ravi <sangana575@gmail.com>
2020-05-14 13:34:51 +0530

    Added a column Modifiedmonth in table Abode

--- Tables/Customer.Abode.sql
+++ Tables/Customer.Abode.sql
@@ -10,7 +10,8 @@ CREATE TABLE [Customer].[Abode]
 [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF__Abode__ModifiedD__4316F928] DEFAULT (getdate()),
 [leafyear] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [newyear] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Modifiedyear] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[Modifiedyear] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Modifiedmonth] [nchar] (10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [Customer].[Abode] ADD CONSTRAINT [PK__Abode__2E2462D880312DA2] PRIMARY KEY CLUSTERED  ([Abode_ID]) ON [PRIMARY]
